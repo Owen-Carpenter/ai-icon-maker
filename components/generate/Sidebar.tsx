fb5662b253684fd3bf45f2a75d8c1d6a42de9574
Owen-Carpenter <owen.carpenter.work@gmail.com>
2025-08-18 19:23:55 -0500

    Tooltips for sidebar

--- components/generate/Sidebar.tsx
+++ components/generate/Sidebar.tsx
@@ -71,6 +71,21 @@ export default function Sidebar() {
             <div className="absolute left-0 top-1/2 transform -translate-x-1 -translate-y-1/2 w-0 h-0 border-t-4 border-b-4 border-r-4 border-transparent border-r-midnight-800"></div>
           </div>
         </div>
+
+        {/* Usage */}
+        <div 
+          onClick={() => handleNavigation('/usage')}
+          className="w-10 h-10 bg-white/10 rounded-lg flex items-center justify-center cursor-pointer hover:bg-white/20 transition-colors group relative"
+        >
+          <svg className="w-5 h-5 text-white/60 group-hover:text-white transition-colors" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
+          </svg>
+          {/* Tooltip */}
+          <div className="absolute left-full ml-3 px-3 py-2 bg-midnight-800 border border-white/20 rounded-lg text-white text-sm whitespace-nowrap opacity-0 group-hover:opacity-100 transition-opacity duration-200 pointer-events-none z-[99999]">
+            Usage & Limits
+            <div className="absolute left-0 top-1/2 transform -translate-x-1 -translate-y-1/2 w-0 h-0 border-t-4 border-b-4 border-r-4 border-transparent border-r-midnight-800"></div>
+          </div>
+        </div>
       </div>
 
       {/* Bottom Section */}
