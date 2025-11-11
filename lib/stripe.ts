acbb36015635314448ef4eada7b48baa3bde9787
Owen Carpenter <owen.carpenter.work@gmail.com>
2025-11-11 13:55:29 -0600

    build fix

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,7 +3,7 @@ import { loadStripe, Stripe as StripeJs } from '@stripe/stripe-js'
 
 // Server-side Stripe instance
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-06-30.basil',
+  apiVersion: '2025-08-27.basil',
   typescript: true,
 })
 
